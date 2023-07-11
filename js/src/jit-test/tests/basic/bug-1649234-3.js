c2417ba628839a9c23341550d637dfa4fa48e7d0
Chris Fallin <chris@cfallin.org>
2023-07-11 00:50:59 -0700

    Some bugfixes: proper argc on Call{Scripted,Native}Function; skip tests for unsupported functionality properly.

--- js/src/jit-test/tests/basic/bug-1649234-3.js
+++ js/src/jit-test/tests/basic/bug-1649234-3.js
@@ -1,5 +1,6 @@
-// |jit-test| exitstatus: 3;
-// |jit-test| skip-if: getJitCompilerOptions()['pbl.enable']
+// |jit-test| exitstatus: 3; skip-if: getBuildConfiguration()['pbl']
+//
+// (skip if PBL is enabled: it doesn't support the debugger)
 
 let debuggerRealm = newGlobal({newCompartment: true});
 debuggerRealm.debuggee = this;
