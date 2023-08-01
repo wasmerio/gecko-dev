ec5ae3ecce3218187caddb6245ead679583a9f8a
Chris Fallin <chris@cfallin.org>
2023-07-31 21:15:28 -0700

    tweak bug743094 limit again to fit with new stack checks

--- js/src/jit-test/tests/auto-regress/bug743094.js
+++ js/src/jit-test/tests/auto-regress/bug743094.js
@@ -6,7 +6,7 @@ var i = 0;
 
 gczeal(2);
 function test() {
-  if (i++ > 250)
+  if (i++ > 200)
     return "function";
   var res = typeof (new test("1")) != 'function';
   return res ? "function" : "string";
