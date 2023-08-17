4f91bcf8494be519a18b6c41904f8e3c562bf923
Chris Fallin <chris@cfallin.org>
2023-08-17 14:22:24 -0700

    PBL part 11 / 18: Add some needed accessors.

--- js/src/vm/NativeObject.h
+++ js/src/vm/NativeObject.h
@@ -408,6 +408,9 @@ class ObjectElements {
 
   bool isPacked() const { return !(flags & NON_PACKED); }
 
+  uint32_t getLength() const { return length; }
+  uint32_t getInitializedLength() const { return initializedLength; }
+
   JS::PropertyAttributes elementAttributes() const {
     if (isFrozen()) {
       return {JS::PropertyAttribute::Enumerable};
@@ -1117,6 +1120,10 @@ class NativeObject : public JSObject {
       return fixedSlots() + slot;
     }
     return slots_ + (slot - fixed);
+  }
+
+  HeapSlot* getSlotsUnchecked() {
+    return slots_;
   }
 
   HeapSlot* getSlotAddress(uint32_t slot) {
