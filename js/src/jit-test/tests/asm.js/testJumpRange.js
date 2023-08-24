205bc4812a1256be712a029eccd7bc57d7db172e
Chris Fallin <chris@cfallin.org>
2023-08-24 13:57:46 -0700

    Reduce some recursion in test suite.

--- js/src/jit-test/tests/asm.js/testJumpRange.js
+++ js/src/jit-test/tests/asm.js/testJumpRange.js
@@ -2,9 +2,9 @@ load(libdir + "asm.js");
 load(libdir + "asserts.js");
 
 var fatFunc = USE_ASM + '\n';
-for (var i = 0; i < 100; i++)
+for (var i = 0; i < 75; i++)
     fatFunc += "function f" + i + "() { return ((f" + (i+1) + "()|0)+1)|0 }\n";
-fatFunc += "function f100() { return 42 }\n";
+fatFunc += "function f75() { return 42 }\n";
 fatFunc += "return f0";
 
 for (let threshold of [0, 50, 100, 5000, -1]) {
@@ -22,5 +22,5 @@ for (let threshold of [0, 50, 100, 5000, -1]) {
     asmLink(asmCompile(USE_ASM + 'function f() {} function g() { f() } function h() { g() } return h'))();
     disableGeckoProfiling();
 
-    assertEq(asmCompile(fatFunc)()(), 142);
+    assertEq(asmCompile(fatFunc)()(), 117);
 }
